289e9b13cb3907a1f45a286b07d770b1feeee92a
MyFrondEndProjects <163176778+MyFrondEndProjects@users.noreply.github.com>
2024-05-12 13:33:36 +0300

    App.js güncellendi

--- Js/App.js
+++ Js/App.js
@@ -2,8 +2,15 @@ const container = document.querySelector("#container");
 const body = document.getElementsByTagName("body");
 const bookcontent = document.querySelector(".book-content");
 const portada = document.querySelector("#portada");
+const prevbtn = document.querySelector(".prevbtn");
+const Gundembtn = document.querySelector("#Gundem");
+const contentlist = document.querySelector(".Contentlist");
+const Ul = document.querySelector(".Ul");
+const bar = document.querySelector(".bar");
 
 
+
+prevbtn.remove();
 /*Oluşturulan elementler*/
 const Home = document.createElement("div");
 const dergi1 = document.createElement("a");
@@ -14,21 +21,30 @@ bookcontent.remove(); //Book content silindi
 // Home özellikleri
 Home.classList.add("Home");
 Home.style.position = "absolute";
-Home.style.width = "100%";
+Home.style.top = "5%";
+Home.style.width = "80%";
 Home.style.height = "100%";
+Home.style.left = "17%";
+Home.style.bottom = "5%";
+Home.style.border = "10px solid Black";
+Home.style.boxShadow = "30px 35px 10px 5px black";
 Home.style.borderRadius = "0.5em";
 Home.style.backgroundImage = "url('/İmg/_\ \(4\).jpeg')";
 container.appendChild(Home); //Home container a eklendi
 
+
+
 // Dergi1 özellikleri
-dergi1.style.width = "8em";
-dergi1.style.height = "10em";
+dergi1.style.width = "13em";
+dergi1.style.height = "18em";
 dergi1.style.position = "absolute";
-dergi1.style.left = "2em";
+dergi1.style.left = "15.9em";
+dergi1.style.top = "0.5em";
 dergi1.style.borderRadius = "0.5em";
 dergi1.style.backgroundImage = "url('İmg/Ekran Görüntüsü - 2024-05-10 11-27-21.png')";
 dergi1.style.cursor = "pointer";
 dergi1.style.backgroundSize = "cover";
+dergi1.style.backgroundPositionY = "2em;"
 Home.appendChild(dergi1); //Dergi1 Home a eklendi
 
 
@@ -37,14 +53,14 @@ Home.appendChild(dergi1); //Dergi1 Home a eklendi
 
 
 // Dergi1 özellikleri
-
 dergi2.style.position = "absolute";
-dergi2.style.width = "8em";
-dergi2.style.height = "10em";
+dergi2.style.width = "13em";
+dergi2.style.height = "18em";
 dergi2.style.backgroundColor = "Blue";
 dergi2.style.backgroundImage = "url('/İmg/_ (2).jpeg')";
 dergi2.style.backgroundSize = "cover";
-dergi2.style.left = "11em";
+dergi2.style.left = "62.6em";
+dergi2.style.top = "0.5em";
 dergi2.cursor = "pointer";
 dergi2.style.borderRadius = "0.5em";
 Home.appendChild(dergi2);//Dergi2 Home a eklendi
@@ -209,7 +225,42 @@ const dergi2open = () => { //Dergi2 fonksiyonu
 }
 
 
+const Contents = () => {
 
+
+	Ul.remove();   //Ul silindi
+	contentlist.textContent = "";
+
+
+
+
+	let Komploteorileri = document.createElement("a");
+	let Komploteorilerili = document.createElement("li");
+
+
+
+	Komploteorileri.textContent = "Komploteorileri";
+
+
+	contentlist.appendChild(prevbtn); // Geri dönme butonu 2. ul ye eklendi
+	bar.appendChild(contentlist);  //Bara 2. Ul eklendi ve tıklanılan elementin içeriği eklendi
+	Komploteorilerili.appendChild(Komploteorileri);
+	contentlist.appendChild(Komploteorilerili);
+
+
+
+
+
+	const prevul = () => {
+		contentlist.remove();
+		bar.appendChild(Ul);
+	}
+	prevbtn.addEventListener("click", prevul);
+}
+
+Gundembtn.addEventListener("click", Contents);
+dergi2.addEventListener("click", dergi2open);  //Dergi 2 click ye eklenen click eventi
+dergi1.addEventListener("click", dergi1open); //Dergi 1 click e eklenen click eventi
 
 
 
